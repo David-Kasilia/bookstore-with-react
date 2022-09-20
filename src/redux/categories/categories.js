6c39a2e5a9aaf34912afdf56e58314103d8fdb17
David-Kasilia <davidkasilia001@gmail.com>
2022-09-20 15:13:32 +0300

    Completed Project

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,36 +1,22 @@
 // Action Type
 const STATUS = 'bookStore/category/STATUS';
-const ADD_CATEGORY = 'bookStore/books/ADD_CATEGORY';
-const REMOVE_CATEGORY = 'bookStore/books/REMOVE_CATEGORY';
 
+const initialState = [];
 // Reducer Function
-const checkStatus = (state = false, action) => {
+const checkStatus = (state = initialState, action) => {
   switch (action.type) {
     case STATUS:
-      return !state;
-    case ADD_CATEGORY:
-      return [...state, action.book];
-
-    case REMOVE_CATEGORY:
-      return state.filter((book) => book !== action.bookId);
+      return 'Under Construction';
 
     default:
       return state;
   }
 };
 
-// Add A Book Category action
+// Check Status for  Category action
 
-export const addBook = (book) => ({
-  type: 'ADD_CATEGORY',
-  book,
-});
-
-// Remove a book Category action
-
-export const removeBook = (bookId) => ({
-  type: 'REMOVE_CATEGORY',
-  bookId,
+export const statusCheck = () => ({
+  type: 'STATUS',
 });
 
 export default checkStatus;
