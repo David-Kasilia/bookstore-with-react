74e4cd20bb93a45415b667ddcd0db8b876b38fc9
David-Kasilia <davidkasilia001@gmail.com>
2022-09-21 12:49:08 +0300

    Added axios and thunk to project

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import bookReducer from './books/books';
 import checkStatus from './categories/categories';
 
@@ -8,6 +9,6 @@ const allReducers = combineReducers({
   status: checkStatus,
 });
 
-const store = configureStore({ reducer: allReducers });
+const store = configureStore({ reducer: allReducers }, applyMiddleware(thunk));
 
 export default store;
